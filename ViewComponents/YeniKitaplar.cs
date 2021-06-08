72ad4d6151e40682c530074cd4460750eabb22c1
muhammedyildiz3465@gmail.com <63956100+muhammedyildizbas@users.noreply.github.com>
2021-06-08 16:14:40 +0300

    Component yapısı ile kitap listesi alanı

---
+++ ViewComponents/YeniKitaplar.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AspNet_Core.Models;
+
+namespace AspNet_Core.ViewComponents
+{
+    public class YeniKitaplar: ViewComponent
+    {
+
+        public IViewComponentResult Invoke()
+    {
+            var kitaplistesi = new List<Kitaplar>
+            {
+               new Kitaplar
+               {
+                   ID=6,
+                   KitapAd="Korku",
+                   Yazar="Zweig"
+               },
+                new Kitaplar
+               {
+                   ID=7,
+                   KitapAd="Anna Karanina",
+                   Yazar="Tolstoy"
+               }
+
+            };
+            return View(kitaplistesi);
+    }
+    }
+}
